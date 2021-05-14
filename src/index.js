25d08b515777dc56eb12e4665bc8c6de3feb34ef
Francis Langit <francis.villanueva.langit@gmail.com>
2021-05-14 13:48:24 +0800

    Create localStorageConfiguration.pushTask method
    Allows task objects to be pushed to a specific project in localStorage.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import './styles/style.css'
 import './bootstrap.bundle.min.js';
 
 const createTask = (text) => {
+    /**Returns object representing task.
+     * 
+     * Args:
+     *  text (string) : Main text of the task.*/
     return { text }
 }
 
 const createProject = (name) => {
+    /**Returns object representing a project.
+     * 
+     * Args:
+     *  name (string) : Name of the project.*/
     let tasks = []
     
     const addTask = (task) => {
+        /**Pushes a task object into the project's tasks array.*/
         tasks.push(task);
     }
 
@@ -18,39 +27,59 @@ const createProject = (name) => {
 }
 
 const localStorageConfiguration = (() => {
-    const _getUserLocalStorage = () => {
+    /**Holds methods allowing for the configuration and usage of the user's 
+     * localStorage "storist" array.*/
+    const _getLocalStorageAsObject = () => {
         if (!localStorage.getItem('storist')) {
             localStorage.setItem('storist', '[]');
         } 
-        return localStorage.getItem('storist');
+        return JSON.parse(localStorage.getItem('storist'));
     }
 
-    const _updateUserLocalStorage = (updatedLocalStorage) => {
-        let updatedLocalStorageString = JSON.stringify(updatedLocalStorage);
-        localStorage.setItem('storist', updatedLocalStorageString);
+    const _updateLocalStorage = (newLocalStorageAsObject) => {
+        /**Updates the user's current localStorage "storist" array. 
+         * 
+         * Args:
+         *  newLocalStorageAsObject (object) : Object to update "storist" in
+         *      localStorage with. Passed as object, not string.*/
+        localStorage.setItem(
+            'storist', JSON.stringify(newLocalStorageAsObject));
     }
 
-    const addProjectToLocalStorage = (project) => {
-        let userLocalStorage = JSON.parse(_getUserLocalStorage());
-        userLocalStorage.push(project);
-        _updateUserLocalStorage(userLocalStorage);
+    const pushProject = (newProjectObject) => {
+        /**Pushes a new project object into user's "storist" array.
+         * 
+         * Args:
+         *  newProjectObject (object) : Project object to be pushed.*/
+        let currentLocalStorage = _getLocalStorageAsObject();
+        currentLocalStorage.push(newProjectObject);
+        _updateLocalStorage(currentLocalStorage);
     }
 
-    return { addProjectToLocalStorage }
+    const pushTask = (newTaskObject, targetProjectName) => {
+        /**Pushes a new task into a targeted project object's tasks array.
+         * 
+         * Args:
+         *  newTaskObject (object) : Task object to be pushed.
+         *  targetProjectName (string) : Name of the target project where
+         *      newTaskObject will be pushed to.*/
+        let currentLocalStorage = _getLocalStorageAsObject();
+        let targetProjectObject = currentLocalStorage.find(projectObject => {
+            return projectObject.name === targetProjectName;
+        });
+        targetProjectObject.tasks.push(newTaskObject);
+        _updateLocalStorage(currentLocalStorage);
+    }
+
+    return { pushProject, pushTask }
 })();
 
 let newProject = createProject('Personal');
-localStorageConfiguration.addProjectToLocalStorage(newProject);
+localStorageConfiguration.pushProject(newProject);
 console.log(localStorage);
+
+let newTask = createTask('Buy flowers for mom.');
+localStorageConfiguration.pushTask(newTask, 'Personal');
+console.log(localStorage);
+
 localStorage.clear();
-
-// Configure task and project generation in local storage
-    // Add projects to local storage
-        // Setup local storage JSON if not present
-        // Add project to storist local storage JSON 
-    // Add tasks to projects in local storage
-        // Setup local storage JSON if not present
-        // Get project that task will be pushed into
-        // Push task into project
-
-// Set up task and project displaying in user interface
